59f14dc3ef6d6620d858e5109def1f2925ed5313
uendibruci1 <dev2@altechsolutions.al>
2025-08-19 15:59:09 +0200

    wip: table filters

---
+++ src/components/shared/loading/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Loader2 } from "lucide-react";
+
+const Loading: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="flex flex-col items-center">
+        <Loader2 className="animate-spin  w-8 h-8 !text-card" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
