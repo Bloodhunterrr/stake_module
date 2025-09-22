4ab39dbdbb1e2d1a02bf42d2cd0e653476474752
luka <lukakosta39@gmail.com>
2025-09-22 18:16:18 +0200

    [WIP]

--- src/components/profile/profile/Messages.tsx
+++ src/components/profile/profile/Messages.tsx
@@ -1,9 +1,12 @@
 'use client'
 import {useLazyGetMessagesQuery , useLazyGetSingleMessageQuery} from "@/services/authApi.ts";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Loading from "@/components/shared/v2/loading.tsx";
 import {formatDateToDMY} from "@/utils/formatDate.ts";
 import {useEffect, useState} from 'react';
 import {cn} from "@/lib/utils.ts";
+import {useAppSelector} from "@/hooks/rtk.ts";
+
 
 import {
     Dialog,
@@ -26,14 +29,10 @@ function Messages() {
         received : []
     })
 
-    // UserContext
     // import {useAppSelector} from "@/hooks/rtk.ts";
-    // const user = useAppSelector((state) => state.auth.user);
-    // console.log(user)
-
-    console.log(messages)
-
+    const user = useAppSelector((state) => state.auth.user);
     const [refresh, setRefresh] = useState<boolean>(true);
+
     useEffect(() => {
         if (refresh) {
             // Fetch sent messages
@@ -68,36 +67,57 @@ function Messages() {
     }, [refresh]);
 
     console.log(isError)
+    console.log(messages)
+
+
 
     return (
         <div className={'bg-background'}>
-            <div className={' min-h-screen text-accent gap-y-2 flex flex-col justify-start pt-2  container mx-auto '}>
-                <p className={'w-fit cursor-pointer border-[1px] rounded-2xl p-2'} onClick={()=>setRefresh(true)}><Trans>Refresh</Trans></p>
-                <div className={cn('w-full h-full p-2' , {
-                    "animate-pulse min-h-12 bg-accent/40" : isFetching
-                })}>
-                    <p><Trans>Received Messages</Trans></p>
-                    { isLoading  ? <Loading/> :
-                        messages?.received.length > 0 ? messages.received.map((message:any) => {
-                            console.log(message.length)
-                            return <SingleMessage message={message} />
-                        }) : <p className={'py-2 pl-2 bg-popover '}><Trans>Not Received any messages yet</Trans></p>
-                    }
-                </div>
-                <div className={cn('w-full h-full  flex flex-col gap-y-2 p-2' , {
-                    "animate-pulse min-h-12 bg-accent/40" : isFetching
-                })}>
-                    <p><Trans>Sent Messages</Trans></p>
-                    <div className={'flex flex-col gap-y-3'}>
-                        { isLoading  ? <Loading/> :
-                            messages?.sent.length > 0 && messages.sent.map((message:any) => {
-                                return <SingleMessage message={message} />
-                            })
-                        }
-                    </div>
+           <section className={'container mx-auto flex flex-row'}>
+               <Tabs defaultValue="received" className="w-full px-2 pt-2">
+                   <TabsList>
+                       <p className={'data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*=\'size-\'])]:size-4 cursor-pointer min-w-20'} onClick={()=>setRefresh(true)}>{isFetching ? <Loading/> : <Trans>Refresh</Trans>}</p>
+                       {
+                           user && user.is_agent && <TabsTrigger value="sent" className={'data-[state=active]:bg-background/30 data-[state=active]:text-background'}><Trans>Sent</Trans></TabsTrigger>
+                       }
+                       <TabsTrigger value="received" className={'data-[state=active]:bg-background/30 data-[state=active]:text-background'}><Trans>Received</Trans></TabsTrigger>
+                   </TabsList>
+                   <TabsContent value="sent">
+                       <div className={'w-full min-h-screen text-accent gap-y-2 flex flex-col justify-start pt-2   '}>
+                           {
+                               user && user.is_agent &&
+                               <div className={cn('w-full h-full  flex flex-col gap-y-2 p-2' , {
+                                   "animate-pulse min-h-12 bg-accent/40" : isFetching
+                               })}>
+                                   <div className={'flex flex-col gap-y-3'}>
+                                       { isLoading  ? <Loading/> :
+                                           messages?.sent.length > 0 && messages.sent.map((message:any) => {
+                                               console.log(message)
+                                               return <SingleMessage message={message} />
+                                           })
+                                       }
+                                   </div>
 
-                </div>
-            </div>
+                               </div>
+                           }
+
+                       </div>
+                   </TabsContent>
+                   <TabsContent value="received">
+                       <div className={'w-full min-h-screen text-accent gap-y-2 flex flex-col justify-start pt-2   '}>
+                           <div className={cn('w-full h-full' , {
+                               "animate-pulse min-h-12 bg-accent/40" : isFetching
+                           })}>
+                               { isLoading  ? <Loading/> :
+                                   messages?.received.length > 0 ? messages.received.map((message:any) => {
+                                       return <SingleMessage message={message} />
+                                   }) : <p className={'py-2 pl-2 bg-popover '}><Trans>Not Received any messages yet</Trans></p>
+                               }
+                           </div>
+                       </div>
+                   </TabsContent>
+               </Tabs>
+           </section>
         </div>
     );
 }
@@ -109,7 +129,7 @@ const SingleMessage = ({message} : {message : any}) => {
     const [fetchSingleMessage , {isError , isLoading}] = useLazyGetSingleMessageQuery()
     const [data, setData] = useState<SingleMessageResponse | undefined>(undefined);
     return (
-        <div className={'flex items-center gap-x-3 py-1 px-1 rounded border'} onClick={()=>{
+        <div className={'flex items-center gap-x-3  p-1 rounded border'} onClick={()=>{
             fetchSingleMessage({id : message.id}).unwrap().then((data : SingleMessageResponse ) =>{
                 setData(data)
             })
