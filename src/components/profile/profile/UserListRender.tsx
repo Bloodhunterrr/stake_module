1f860438dcce390dc18dd7ab41eb44fcfc65159d
Bloodhunterrr <temflavio@gmail.com>
2025-09-23 16:32:24 +0200

    [translation] users

--- src/components/profile/profile/UserListRender.tsx
+++ src/components/profile/profile/UserListRender.tsx
@@ -135,7 +135,7 @@ function UserItem({
 function LoggedUser({data} : { data : any }){
     return <div className={'border-x text-white text-xs border-t border-popover'}>
         <div className={'p-2 border-b bg-chart-2 border-popover'}>
-            My balance
+            <Trans>My balance</Trans>
         </div>
         <div className={''}>
             <div className={cn('grid w-full text-end mr-12 grid-cols-4' , {
@@ -297,25 +297,20 @@ function UserListRender() {
                 </div>
                 <div className={'text-white text-xs py-2 '}>
                     <div className={'p-2 border-b flex flex-row items-center justify-between bg-chart-2 border-popover'}>
-                        <Button
-                            onClick={()=>{
+                        <Button onClick={()=>{
                                 if(combinedData?.user) {
                                     handleTreeStatusChange({value : true , id : combinedData?.user?.id})
                                 }
-                            }}
-
-                            className={'p-0 size-3 bg-card hover:bg-card'}></Button>
-                        <p>User Balance</p>
-                        <Button
-                            onClick={()=>{
+                            }} className={'p-0 size-3 bg-card hover:bg-card'}></Button>
+                        <Trans>User Balance</Trans>
+                        <Button onClick={()=>{
                                 if(combinedData?.user) {
                                     handleTreeStatusChange({value : false , id : combinedData?.user.id})
                                 }
-                            }}
-                            className={'p-0 size-3 bg-destructive hover:bg-destructive'}></Button>
+                            }} className={'p-0 size-3 bg-destructive hover:bg-destructive'}></Button>
                     </div>
                     <div className={'grid grid-cols-4 border-t bg-chart-2 border-x border-popover py-2 px-1'}>
-                        <div>Username</div>
+                        <Trans>Username</Trans>
                         <div className={'text-center lg:text-start'}>EUR</div>
                         <div className={'text-center lg:text-start'}>USD</div>
                         <div className={'text-end'}></div>
@@ -337,7 +332,7 @@ function UserListRender() {
                             );
                         })
                     ) : (
-                        <p>There is no other user</p>
+                        <Trans>There is no other user</Trans>
                     )}
                 </div>
             </div>
