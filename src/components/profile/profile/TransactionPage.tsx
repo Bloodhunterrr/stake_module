ac3a7486dca17b74a266ab12efe68bedfef814d8
izet <selimajizet21@gmail.com>
2025-09-27 12:05:13 +0200

    [add] username to reports table

--- src/components/profile/profile/TransactionPage.tsx
+++ src/components/profile/profile/TransactionPage.tsx
@@ -312,7 +312,7 @@ function TicketPage() {
         }}
       >
         <p className="w-1/3 h-full flex items-center truncate line-clamp-1 justify-start text-start shrink-0">
-          {item?.name !== "" ? item.name : "------"} ({item.total_played})
+          {item?.username} ({item.total_played})
         </p>
         <p className="w-full h-full flex items-center justify-center">
           {!doDecimal ? (item.total_stake /100).toFixed(2) : item.total_stake.toFixed(2)}
